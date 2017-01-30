991cd1d8478268ef471eb24cb8afe63bf8b6b8a9
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2017-01-29 21:22:25 -0300

    Add react-native-svg and WUCLogo component

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,6 +5,7 @@ import {
   TextInput,
 } from 'react-native';
 import WUCText from './WUCText';
+import WUCLogo from './WUCLogo';
 
 import style from '../styles/Login';
 import commonStyle from '../styles/common';
@@ -12,6 +13,9 @@ import commonStyle from '../styles/common';
 export default function Login() {
   return (
     <KeyboardAvoidingView style={style.wrapper} behavior="padding">
+      <View style={style.logoHeader}>
+        <WUCLogo size="90" />
+      </View>
       <View style={[commonStyle.innerBox, style.loginBox]}>
         <WUCText title>Iniciar Sesión</WUCText>
         <WUCText>Inicia sesión usando tu usuario y contraseña uc</WUCText>
@@ -19,6 +23,7 @@ export default function Login() {
         <TextInput style={{ height: 40 }} placeholder="usuario" />
         <TextInput style={{ height: 40 }} placeholder="contraseña" />
       </View>
+      <View style={style.bottomSpacer} />
     </KeyboardAvoidingView>
   );
 }
