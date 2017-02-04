05772e2955c111574cec14217625286de57c221d
Pedro Pablo Aste Kompen <wachunei@Renaw.local>
2017-02-04 12:16:49 -0300

    feat: Add react-native-router-flux as navigator

---
+++ src/scenes/LoginScene.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  Scene,
+} from 'react-native-router-flux';
+
+import Login from '../components/Login';
+
+export default (
+  <Scene
+    key="Login"
+    component={Login}
+    initial
+  />
+);
