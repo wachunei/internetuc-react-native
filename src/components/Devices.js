c0ad62a4e4f0b727a4c945447e2cc493b2f416d3
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2017-02-09 22:19:28 -0300

    Add initial scroll view layout and components

---
+++ src/components/Devices.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  View,
+} from 'react-native';
+import WUCText from './WUCText';
+import WUCLogo from './WUCLogo';
+
+import commonStyle from '../styles/common';
+
+export default function Devices() {
+
+  return (
+    <View style={commonStyle.viewWrapper}>
+      <View style={[commonStyle.innerBox, commonStyle.itemInnerBox]}>
+      <WUCText title>Dispositivos</WUCText>
+      </View>
+    </View>
+  );
+}
+
+Devices.propTypes = {};
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,58 @@
 import React from 'react';
 import {
-  Text,
   View,
+  ScrollView,
+  StyleSheet,
+  Dimensions,
 } from 'react-native';
+
+// TODO these should be containers :)
+import Devices from './Devices';
+import Form from './Form';
+import About from './About';
 
 import style from '../styles/Main';
 
-export default function Main() {
-  return (
-    <View style={style.wrapper}>
-      <Text>Main</Text>
-    </View>
-  );
+
+const window = Dimensions.get('window');
+
+const windowStyle = StyleSheet.create({
+  item: {
+    flex: 1,
+    height: window.height,
+    width: window.width,
+  },
+});
+
+export default class Main extends React.Component {
+
+  componentDidMount() {
+    this.scrollView.scrollTo({ x: window.width, animated: false });
+  }
+
+  render() {
+    return (
+      <ScrollView
+        ref={sv => (this.scrollView = sv)}
+        horizontal
+        pagingEnabled
+        directionalLockEnabled
+        bounces={false}
+        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
+        decelerationRate="fast"
+        style={style.wrapper}
+      >
+        <View style={windowStyle.item}>
+          <Form />
+        </View>
+        <View style={windowStyle.item}>
+          <Devices />
+        </View>
+        <View style={windowStyle.item}>
+          <About />
+        </View>
+      </ScrollView>
+    );
+  }
 }
