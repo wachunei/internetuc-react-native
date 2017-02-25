153925e81808136d77b3c494df70309f927577a5
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2017-02-25 12:27:45 -0300

    Add feature to get user full name

--- src/utils/UCLogin/utils.js
+++ src/utils/UCLogin/utils.js
@@ -1,4 +1,5 @@
 export const URL = {
   login: 'http://webcurso.uc.cl/direct/session',
+  current: 'http://webcurso.uc.cl/direct/user/current.json',
   logout: 'http://webcurso.uc.cl/portal/logout',
 };
