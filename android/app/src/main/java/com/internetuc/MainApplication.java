3c75e755bfefc64ad9ab85d16b3ae792f1a49979
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2017-08-14 23:14:03 -0300

    Upgrade most dependencies

--- android/app/src/main/java/com/internetuc/MainApplication.java
+++ android/app/src/main/java/com/internetuc/MainApplication.java
@@ -3,7 +3,7 @@ package com.internetuc;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.i18n.reactnativei18n.ReactNativeI18n;
+import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactInstanceManager;
@@ -27,7 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ReactNativeI18n(),
+            new RNI18nPackage(),
             new VectorIconsPackage(),
             new SvgPackage()
       );
